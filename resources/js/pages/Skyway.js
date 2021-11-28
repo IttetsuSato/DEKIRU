7129946855c4ec9e651fd7dd13ad30ed4f1c5a61
ittetsu <ittetsusato@gmail.com>
2021-11-28 15:07:41 +0900

    video終了

--- resources/js/pages/Skyway.js
+++ resources/js/pages/Skyway.js
@@ -6,12 +6,16 @@ const peer = new Peer({key: '95ba327e-64d1-4c05-8f9f-ad00ac893e07'});
 function Skyway() {
   const [myId, setMyId] = useState('');
   const [callId, setCallId] = useState('');
+  const [localMessage, setLocalMessage] = useState('');
+  const [messages, setMessages] = useState('');
   //DOM要素の取得
   const localVideo = useRef(null);
   const remoteVideo = useRef(null);
+  const sendTrigger = useRef(null);
 
   //open: SkyWayサーバーとの接続が成功したタイミングで発火
   peer.on('open', () => {
+    setMessages(messages + `=== サーバー接続成功 ===\n`);
     //PeerID取得
     setMyId(peer.id);
     // カメラ映像取得
@@ -33,6 +37,12 @@ function Skyway() {
     mediaConnection.on('stream', async stream => {
       remoteVideo.current.srcObject = stream;
     });
+
+    //close: 接続が切れたときに発火
+    mediaConnection.once('close', () => {
+      remoteVideo.current.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.current.srcObject = null;
+    });
   });
 
   //error: 何らかのエラーで発火
@@ -40,20 +50,33 @@ function Skyway() {
     alert(err.message);
   });
 
-  //close: 接続が切れたときに発火
-  peer.on('close', () => {
-    alert('通信が切断しました。');
-  });
-
   const makeCall = () => {
+    if (!peer.open) {
+      return;
+    }
     //peer.call()で接続 => mediaConnectionに接続相手の情報が帰ってくる
     const mediaConnection = peer.call(callId, localVideo.current.srcObject);
-    console.log('相手にコール')
     //相手の映像をvideo要素にセット
     mediaConnection.on('stream', async stream => {
       remoteVideo.current.srcObject = stream;
       await remoteVideo.current.play().catch(console.error);
     });
+
+    //close: 接続が切れたときに発火
+    mediaConnection.once('close', () => {
+      remoteVideo.current.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.current.srcObject = null;
+    });
+  }
+
+
+  //通話終了処理
+  const endCall = () => {
+    peer.destroy();
+  }
+
+  const onClickSend = () => {
+
   }
 
   return (
@@ -69,17 +92,28 @@ function Skyway() {
                         autoPlay muted playsInline>
                         </video>
                       </div>
-                      <div>{myId}</div>
+
                       <div>
+                        <div>YOUR ID:{myId}</div>
                         <input value={callId} onChange={e => setCallId(e.target.value)}></input>
                         <button onClick={makeCall}>発信</button>
+                        <button onClick={endCall}>終了</button>
                       </div>
+
                       <div>
-                      <video
-                      width="400px"
-                      ref={remoteVideo}
-                      autoPlay muted playsInline>
-                      </video>
+                        <input value={localMessage} onChange={e => setLocalMessage(e.target.value)}></input>
+                        <button
+                        ref={sendTrigger}
+                        onClick={onClickSend}>送信
+                        </button>
+                      </div>
+
+                      <div>
+                        <video
+                        width="400px"
+                        ref={remoteVideo}
+                        autoPlay muted playsInline>
+                        </video>
                       </div>
                   </div>
               </div>
