dc879bf19a34f4510b40e89c4184b89075549537
ittetsu <ittetsusato@gmail.com>
2021-12-11 00:37:51 +0900

    ミュート、画面オフ機能を実装

--- resources/js/pages/Skyway.js
+++ resources/js/pages/Skyway.js
@@ -7,11 +7,18 @@ import Paper from '@mui/material/Paper'
 import CallIcon from '@mui/icons-material/Call';
 import CallEndIcon from '@mui/icons-material/CallEnd';
 import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import VideocamOffIcon from '@mui/icons-material/VideocamOff';
 
 function Skyway(){
   const peer = new Peer({key: '95ba327e-64d1-4c05-8f9f-ad00ac893e07'});
   const [remoteVideo, setRemoteVideo] = useState([]);
   const [localStream, setLocalStream] = useState('');
+  const [isConnected, setIsConnected] = useState(false); //false: 接続なし, true: 通話中
+  const [isMuted, setIsMuted] = useState(true); //false: ミュート
+  const [isOffScreen, setIsOffScreen] = useState(true); //false: 画面オフ
   const localVideoRef = useRef(null);
 
   
@@ -20,20 +27,23 @@ function Skyway(){
   
   //useEffect実行時、自身のカメラ映像取得
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    changeStream();
+  }, []);
+
+  const changeStream = () => {
+    navigator.mediaDevices.getUserMedia({video: isOffScreen, audio: isMuted})
     .then( stream => {
       // 成功時にvideo要素にカメラ映像をセット
       setLocalStream(stream);
-      if (localVideoRef.current) {
-        localVideoRef.current.srcObject = stream;
-        localVideoRef.current.play().catch((e) => console.log(e));
-      }
+      localVideoRef.current.srcObject = stream;
+      localVideoRef.current.play().catch((e) => console.log(e));
     }).catch( error => {
       // 失敗時にはエラーログを出力
       console.error('mediaDevice.getUserMedia() error:', error);
       return;
     });
-  }, []);
+    console.log(`video: ${isOffScreen}, audio: ${isMuted}`)
+  }
   
   const onStart = () => {
     if(peer){
@@ -61,6 +71,7 @@ function Skyway(){
     //open: SkyWayサーバーとの接続が成功したタイミングで発火
     room.once("open", () => {
       messages.textContent += '=== ルームに参加しました ===\n';
+      setIsConnected(true);
     });
 
     //peerJoin: 誰かがroomに参加したときに発火
@@ -104,6 +115,7 @@ function Skyway(){
           return false;
         })
       );
+      setIsConnected(false);
     });
 
     //送信ボタンの処理
@@ -139,11 +151,16 @@ function Skyway(){
     }
   };
 
+  const onClickMute = () =>{
+
+  }
+  
+
   return (
     <div>
       <Box sx={{ width: '100%', 'backgroundColor': '#333', position: 'relative'}}>
         {/* 相手の画面 */}
-        <Box sx={{ width: '75%', display: 'flex', 'justifyContent': 'center', margin: 'auto'}}>
+        <Box sx={{ height: '100vh', display: 'flex', 'justifyContent': 'center', margin: 'auto'}}>
           {castVideo()}
         </Box>
         {/* チャット */}
@@ -166,6 +183,8 @@ function Skyway(){
           </Box>
           <Button id="call-trigger" color="primary" variant="contained" onClick={() => onStart()} startIcon={<CallIcon />}>開始</Button>
           <Button id="leave-trigger" color="secondary" variant="contained" startIcon={<CallEndIcon />}>終了</Button>
+          <Button color="primary" variant="contained" onClick={() => {setIsMuted(prev => !prev); changeStream()}}>{isMuted ? <MicIcon /> : <MicOffIcon />}</Button>
+          <Button color="primary" variant="contained" onClick={() => {setIsOffScreen(prev => !prev); changeStream()}}>{isOffScreen ? <VideocamIcon /> : <VideocamOffIcon />}</Button>
         </Box>
       </Box>
     </div>
